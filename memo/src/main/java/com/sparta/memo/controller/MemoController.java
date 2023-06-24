1e7c867e1b2b9b42c26dc5733ad2e27ecb637161
YiHwanRyu <yihwandev@gmail.com>
2023-06-24 09:03:42 +0900

    [UPDATE] Memo Project: Update, Delete 구현

--- memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -14,6 +14,7 @@ public class MemoController {
 
     //아직 데이터베이스 연결 전이어서 Map 이용
     private final Map<Long, Memo> memoList = new HashMap<>();
+
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // RequestDto -> Entity(데이터베이스 교환 객체)
@@ -40,7 +41,32 @@ public class MemoController {
         return responseList;
     }
 
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
 
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 메모가 DB 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모를 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
 
 
 }
