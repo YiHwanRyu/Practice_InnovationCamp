892703b89bb37d366c7738fd53f84173e19cfd4a
YiHwanRyu <yihwandev@gmail.com>
2023-06-26 13:06:51 +0900

    [UPDATE] Memo Project: JPA Auditing 적용하기

--- memo/src/main/java/com/sparta/memo/MemoApplication.java
+++ memo/src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,8 +2,10 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JpaAuditing 기능을 사용하겠다.
 public class MemoApplication {
 
     public static void main(String[] args) {
