57fb91110fe08638895c278510fbbad20541dfb2
YiHwanRyu <yihwandev@gmail.com>
2023-06-30 19:29:33 +0900

    [UPDATE] 정규표현식 과제 완료

--- spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ public class SignupRequestDto {
     private String username;
     @NotBlank
     private String password;
-    @Email
+    @Pattern(regexp = "^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-z0-9-]+(\\.[a-z0-9]+)*(\\.[A-Za-z]{2,5})$")// \\는 특수문자 표시, 뒤에 * 없어도 되는 거
     @NotBlank
     private String email;
     private boolean admin = false;
