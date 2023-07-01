af7b0ea8473d1474cee05440904a11f623d31ef1
YiHwanRyu <yihwandev@gmail.com>
2023-07-01 10:01:30 +0900

    [UPDATE] RestTemplate-client: postForEntity 요청

--- spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -1,6 +1,7 @@
 package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
+import com.sparta.springresttemplateclient.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -61,7 +62,23 @@ public class RestTemplateService {
     }
 
     public ItemDto postCall(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/post-call/{query}") // PathVariable 방식으로 만들고자하면
+                .encode()
+                .build()
+                .expand(query)
+                .toUri();
+        log.info("uri = " + uri);
+
+        User user = new User("Robbie", "1234");
+
+        ResponseEntity<ItemDto> responseEntity = restTemplate.postForEntity(uri, user, ItemDto.class); // 요청uri, 요청, 응답받을 형식
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> exchangeCall(String token) {
